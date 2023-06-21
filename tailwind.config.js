21fcea6cde4ad75a98538a6d88f276a2596faaf1
Yuta Nishikawa <kinkitunekin@gmail.com>
2023-06-21 13:38:11 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      "tutorial-bg": "url('../src/img/main-bg.jpg')",
+    },
+  },
+  plugins: [],
+}
+
